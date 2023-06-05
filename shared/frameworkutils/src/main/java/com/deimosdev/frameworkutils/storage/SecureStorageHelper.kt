4247d74ff54a8ba316f8384315ac504f1b0fcfb6
patrickessiam <patrick.essiam@deimos.co.za>
2023-06-05 17:42:06 +0000

    Add requiresApi annotation for compatibility

--- shared/frameworkutils/src/main/java/com/deimosdev/frameworkutils/storage/SecureStorageHelper.kt
+++ shared/frameworkutils/src/main/java/com/deimosdev/frameworkutils/storage/SecureStorageHelper.kt
@@ -1,12 +1,15 @@
 package com.deimosdev.frameworkutils.storage
 
 import android.content.Context
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.security.crypto.EncryptedSharedPreferences
 import androidx.security.crypto.MasterKeys
 
 open class SecureStorageHelper(context: Context) {
 
     private val fileName = "secure_storage"
+    @RequiresApi(Build.VERSION_CODES.M)
     private val sharedPreferences = EncryptedSharedPreferences.create(
         fileName,
         MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC),
@@ -15,10 +18,12 @@ open class SecureStorageHelper(context: Context) {
         EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
     )
 
+    @RequiresApi(Build.VERSION_CODES.M)
     fun saveString(key: String, value: String) {
         sharedPreferences.edit().putString(key, value).apply()
     }
 
+    @RequiresApi(Build.VERSION_CODES.M)
     fun retrieveString(key: String, defaultValue: String? = null): String? {
         return sharedPreferences.getString(key, defaultValue)
     }
